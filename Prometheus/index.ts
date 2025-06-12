81c0016e84970655b239d5c1ad5214c8ffa47861
Aryan Rathore <aryanrathoreop@gmail.com>
2025-06-12 20:21:25 +0530

    Prometheus basic repsonse time tracking using histogram and counter with ui

--- Prometheus/index.ts
+++ Prometheus/index.ts
@@ -1,6 +1,6 @@
 import Express, { type NextFunction, type Request, type Response } from "express";
 import client from 'prom-client'
-
+import cors from 'cors'
 const requestCounter = new client.Counter({
     name:'http_request_total',
     help:"Getting Http server request state",
@@ -12,9 +12,13 @@ const requestBucket = new client.Histogram({
     labelNames:['method','status_code','route'],
     buckets:[0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000]
 })
+const activeRequest = new client.Gauge({
+    name:"Active_Http_Requests",
+    help:"Tracks the number of active requests", 
+})
 function middleware(req: Request, res: Response, next: NextFunction) {
     const startTime = Date.now();
-
+    activeRequest.inc();
     res.on('finish', () => {
         const route = req.route?.path || req.path || req.url;
         console.log(`Time it took is ${Date.now() - startTime} ms for method ${req.method} for route ${route}`);
@@ -29,6 +33,7 @@ function middleware(req: Request, res: Response, next: NextFunction) {
         status_code:res.statusCode,
         route
          },Date.now() - startTime)
+         activeRequest.dec();
     });
  
 
@@ -38,19 +43,18 @@ function middleware(req: Request, res: Response, next: NextFunction) {
 const app = Express();
 app.use(Express.json());
 app.use(middleware);
+app.use(cors())
 app.get('/',middleware,(req,res)=>{
     console.log("Simple bun app is working 2")
-    res.send("Hello World")
+    res.send("Hello World  ")
 })
 
-app.get('/cpu', middleware, (req:Request,res:Response)=>{
-    for(let i = 0;i< Math.random()*1000 ;i++){
-        i++;
-    }
+app.get('/cpu', async (req:Request,res:Response)=>{
+    await new Promise((resolve)=> setTimeout(resolve, 1000*Math.random()))
     res.send("/cpu end point hit")
 })
-app.get('/user', middleware, (req:Request,res:Response)=>{
-    res.send("/user end point hit")
+app.get('/user/:userId', (req:Request,res:Response)=>{
+    res.send(`/user end point hit with ${req.params.userId}`)
 })
 app.get('/metrics',async (req,res)=>{
 const metrics = await client.register.metrics();
@@ -58,6 +62,6 @@ res.set('Content-Type', client.register.contentType)
 res.end(metrics);
 })
 
-app.listen(80,()=>{
+app.listen(3000,()=>{
     console.log("Server listening on port 80")
 })
